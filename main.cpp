86401e8a82340b0fa23af721df3bcd2013f1b335
Robert Jaremczak <robert@mindpart.com>
2019-09-17 22:24:05 +0200

    palette eliminated in favor of stylesheets

--- main.cpp
+++ main.cpp
@@ -2,35 +2,17 @@
 
 #include <QApplication>
 #include <QStyleFactory>
-
-static void initStyle() {
-    QApplication::setStyle(QStyleFactory::create("Fusion"));
-}
-
-static void initPalette() {
-    QPalette palette;
-    palette.setColor(QPalette::Window, QColor(53,53,53));
-    palette.setColor(QPalette::WindowText, Qt::white);
-    palette.setColor(QPalette::Base, QColor(53,53,53));
-    palette.setColor(QPalette::AlternateBase, QColor(45,45,45));
-    palette.setColor(QPalette::Text, Qt::white);
-    palette.setColor(QPalette::Button, QColor(53,53,53));
-    palette.setColor(QPalette::ButtonText, Qt::white);
-    palette.setColor(QPalette::BrightText, Qt::red);
-
-    palette.setColor(QPalette::Highlight, QColor(0xfd, 0x6a, 0x02).lighter());
-    palette.setColor(QPalette::HighlightedText, Qt::black);
-    palette.setColor(QPalette::Disabled, QPalette::Text, Qt::darkGray);
-    palette.setColor(QPalette::Disabled, QPalette::ButtonText, Qt::darkGray);
-    QApplication::setPalette(palette);
-}
-
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
-    initStyle();
-    initPalette();
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
     QApplication a(argc, argv);
+
+    QFile qss(":/main.qss");
+    qss.open( QFile::ReadOnly);
+    a.setStyleSheet(qss.readAll());
+
     MainWindow w;
     //QObject::connect(&a, &QCoreApplication::aboutToQuit, &w, &MainWindow::prepareToQuit);
     w.show();
--- mo6502.pro
+++ mo6502.pro
@@ -24,6 +24,8 @@ FORMS += \
     cpuwidget.ui \
     mainwindow.ui
 
+RESOURCES += resources.qrc
+
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
