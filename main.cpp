45de184051b235162d408c093be166da4aeae742
Robert Jaremczak <rjaremczak@gmail.com>
2020-03-13 19:05:31 +0100

    content of main.qss moved to main.cpp, migration to cmake complete, qmake files removed

--- main.cpp
+++ main.cpp
@@ -48,9 +48,47 @@ int main(int argc, char* argv[]) {
   QApplication app(argc, argv);
   QApplication::setStyle(QStyleFactory::create("Fusion"));
 
-  QFile qss(":/main.qss");
-  qss.open(QFile::ReadOnly);
-  app.setStyleSheet(qss.readAll());
+  app.setStyleSheet(R"(
+    * {
+        background-color: rgb(63,67,67);
+        color: white;
+        selection-color: black;
+        selection-background-color: orange;
+        alternate-background-color: rgb(41,41,41);
+    }
+
+    *:disabled {
+        color: gray;
+    }
+
+    QAbstractButton:checked {
+        color: black;
+        background-color: orange;
+    }
+
+    QDockWidget {
+    }
+
+    QDockWidget::title {
+    }
+
+    QMainWindow::separator {
+        /*
+        background: rgb(41,41,41);
+        width: 2px;
+        height: 2px;
+        */
+    }
+
+    QMainWindow::separator:hover {
+        /* background: orange; */
+    }
+
+    QStatusBar {
+        /* border-top: 1px solid rgb(41,41,41); */
+    }
+
+  )");
 
   MainWindow mainWindow;
   QRect scr = QGuiApplication::primaryScreen()->geometry();
