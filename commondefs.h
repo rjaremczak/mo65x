655191f8a0d85dc0c73be61d315033ea51dc8891
Robert Jaremczak <rjaremczak@gmail.com>
2020-03-13 18:37:44 +0100

    steady clock is more precise with MinGW than high_resolution_clock

--- commondefs.h
+++ commondefs.h
@@ -9,6 +9,6 @@
 using Address = uint16_t;
 using Data = std::vector<uint8_t>;
 using FileOperationCallBack = std::function<void(uint64_t)>;
-using PreciseClock = std::chrono::high_resolution_clock;
+using PreciseClock = std::chrono::steady_clock; // as high_resolution_clock with MinGW is less accurate !!!
 using Duration = std::chrono::nanoseconds;
 using Frequency = uint32_t;
