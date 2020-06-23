28f50ea5ca305d5c49b808389253e68b92516dc7
Robert Jaremczak <rjaremczak@gmail.com>
2020-06-23 20:48:12 +0200

    cmake builds and runs the project yet needs more work...

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,11 @@
-#include <gtest/gtest.h>
 #include "app.h"
 #include "commondefs.h"
 #include "config.h"
 #include "emulatorstate.h"
 #include "filedatastorage.h"
-#include "test/assemblertest.h"
-#include "test/flagstest.h"
-#include "test/instructionstest.h"
 
-int gtest(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
+App app;
 
 int main(int, char**) {
-#ifdef RUN_TESTS
-  return gtest(argc, argv);
-#else
-  auto app = std::make_unique<App>();
-  return app->run();
-#endif
+  return app.run();
 }
